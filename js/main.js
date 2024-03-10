8c810cee357faf4cdf830c382c1d1e8f906a02a9
TomvanD298 <150897738+TomvanD298@users.noreply.github.com>
2024-03-10 15:21:42 +0100

    bleep

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function showPopUp (_){
+    document.getElementById("js--popup").style.display='block'
+}
+
+function hidePopUp (_){
+    document.getElementById("js--popup").style.display='None'
+}
+
+function showPhoto (_){
+    document.getElementById("js--photo").style.display='block'
+}
+
+function hidePhoto (_){
+    document.getElementById("js--photo").style.display='None'
+}
+
+function showPhoto2 (_){
+    document.getElementById("js--photo2").style.display='block'
+}
+
+function hidePhoto2 (_){
+    document.getElementById("js--photo2").style.display='None'
+}
+
+// Code geholpen door Daniel van Droffelaar // 
